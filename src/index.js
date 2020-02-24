c40579826a7437cb7fc1422a1abe8daa01721632
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2020-02-24 21:31:48 +0300

    init: submit task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+  if (n < 0) {
+    let arr = String(n).split('');
+    arr.splice(0, 1);
+    let str = arr.reverse().join('');    
+    return Number(str);
+  } else {
+    let arr = String(n).split('');
+    let str = arr.reverse().join('');    
+    return Number(str);
+  };  
+};
